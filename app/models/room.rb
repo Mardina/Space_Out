6aeeea94faac31fa2d889ccec3979a8e6e58eb99
Melinda Batang <meeelinda.b@gmail.com>
2021-12-02 22:40:56 +1100

    created booking and room models

---
+++ app/models/room.rb
@@ -0,0 +1,10 @@
+class Room < ApplicationRecord
+  belongs_to :user
+  has_many :bookings, dependent: :destroy
+  validates :title, length: { maximum: 25 }, presence: true
+  validates :address, presence: true
+  validates :description, length: { maximum: 500 }, presence: true
+  validates :capacity, presence: true, numericality: { only_integer: true }
+  validates :availability_start, presence: true, timeliness: { type: :datetime }
+  validates :availability_end, presence: true, timeliness: { type: :datetime }
+end

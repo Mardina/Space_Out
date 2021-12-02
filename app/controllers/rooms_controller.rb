856cb0f037e317e1877528c0d6f2fd868d3d5a12
Elana Strijbis <ehgestrijbis@gmail.com>
2021-12-02 21:15:59 +1100

    Added the destroy dependent

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,6 @@
 class RoomsController < ApplicationController
   before_action :set_room, only: [:show, :edit, :update]
-  before_action :authenticate_user!, except [:show]
+  before_action :authenticate_user!, except: [:show]
   def index
     @rooms = current_user.rooms
   end

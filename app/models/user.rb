19373762f01033cbc1fba3f0ae8d40be06c193dc
Melinda Batang <meeelinda.b@gmail.com>
2021-12-02 21:04:27 +1100

    added dependent destroy on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
-
-  validates :name, presence: true, length: {maximum: 50}
-  validates :email, presence: true, :uniqueness => true
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 8 }
 
-  has_many :rooms
+  has_many :rooms, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,

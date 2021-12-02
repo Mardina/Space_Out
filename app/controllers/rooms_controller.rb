61bd3b9ab763f7a52b18c112851680d1b03a4f76
Elana Strijbis <ehgestrijbis@gmail.com>
2021-12-02 19:13:30 +1100

    Created the rooms_controller

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,43 @@
+class RoomsController < ApplicationController
+  before_action :set_room, only: [:show, :edit, :update]
+  before_action :authenticate_user!, except [:show]
+  def index
+    @rooms = current_user.rooms
+  end
+
+  def show
+  end
+
+  def new
+    @room = current_user.rooms.build
+  end
+
+  def create
+    @room = current_user.rooms.build(room_params)
+
+    if @room.save
+      redirect_to @room, notice: "Saved"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @room.update(room_params)
+      redirect_to @room, notice: "Updated..."
+    else
+      render :edit
+  end
+
+  pivate
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params.require(:room).permit(:address, :description, :capacity, :booking_id, :user_id, :availability_start, :availability_end, :images)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: 'pages#home'
 
   get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:show]
+  resources :rooms
 end

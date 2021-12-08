230a5cbb995fdf8f3967f3920a3499632b48f52f
Melinda Batang <meeelinda.b@gmail.com>
2021-12-08 19:44:18 +1100

    added image URL

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,6 +30,7 @@ room1 = Room.create!(
   user_id: elana.id,
   availability_start: "01-02-2023 10:00",
   availability_end: "01-02-2023 13:30",
+  image_url: 'https://images.unsplash.com/photo-1610374792793-f016b77ca51a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
 )
 room2 = Room.create!(
   title: 'Funky Meetings',
@@ -39,6 +40,7 @@ room2 = Room.create!(
   user_id: elana.id,
   availability_start: "01-02-2023 10:00",
   availability_end: "01-02-2023 13:30",
+  image_url: 'https://images.unsplash.com/photo-1595329088732-d3b286981166?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
 )
 room3 = Room.create!(
   title: 'Wooden Room',
@@ -48,6 +50,7 @@ room3 = Room.create!(
   user_id: elana.id,
   availability_start: "01-02-2023 10:00",
   availability_end: "01-02-2023 13:30",
+  image_url: 'https://images.unsplash.com/photo-1618543005334-b0904ecd4441?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1168&q=80'
 )
 room4 = Room.create!(
   title: 'Metallic Meeting Room',
@@ -57,6 +60,7 @@ room4 = Room.create!(
   user_id: elana.id,
   availability_start: "01-02-2023 10:00",
   availability_end: "01-02-2023 13:30",
+  image_url: 'https://images.unsplash.com/photo-1503418895522-46f9804cda40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
 )
 room5 = Room.create!(
   title: 'Super Modern Meeting Room',
@@ -66,33 +70,37 @@ room5 = Room.create!(
   user_id: mardina.id,
   availability_start: "01-12-2021 10:00",
   availability_end: "01-02-2023 13:30",
+  image_url: 'https://images.unsplash.com/photo-1462826303086-329426d1aef5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
 )
 room6 = Room.create!(
-  title: 'Eclectic Meetings',
+  title: 'Playroom Meetings',
   address: 'Brunswick, Melbourne',
   description: 'The room is done in a country theme in warm colors and overall looks very eclectic.',
   capacity: '6',
   user_id: mardina.id,
   availability_start: "01-12-2021 10:00",
   availability_end: "01-02-2023 13:30",
+  image_url: 'https://images.unsplash.com/photo-1557804500-7a58fbcd4d1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
 )
 room7 = Room.create!(
-  title: 'High-End Room',
+  title: 'Vogue Room',
   address: 'Mackenzie St',
-  description: 'Amazing meeting room. Great for privacy and secret meetings',
+  description: 'Perfect urban meeting space for modern fashionista.',
   capacity: '4',
   user_id: mardina.id,
   availability_start: "01-12-2021 10:00",
   availability_end: "01-02-2023 13:30",
+  image_url: 'https://images.unsplash.com/photo-1505409859467-3a796fd5798e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
 )
 room8 = Room.create!(
   title: 'Quirky Meetings',
   address: 'Williamson St, Melbourne',
-  description: 'The room is done in cool colors and overall has a quirky look.',
+  description: 'The outdoor space is done in cool colors and overall has a quirky look.',
   capacity: '8',
   user_id: mardina.id,
   availability_start: "01-12-2021 10:00",
   availability_end: "01-02-2023 13:30",
+  image_url: 'https://images.unsplash.com/photo-1520694420531-1d8eca53d7bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
 )
 room9 = Room.create!(
   title: 'Hot Desert Meetings',
@@ -102,6 +110,7 @@ room9 = Room.create!(
   user_id: melinda.id,
   availability_start: "01-12-2021 10:00",
   availability_end: "01-02-2023 13:30",
+  image_url: 'https://images.unsplash.com/photo-1549944850-84e00be4203b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1068&q=80'
 )
 room10 = Room.create!(
   title: 'Mystery Room',
@@ -111,6 +120,7 @@ room10 = Room.create!(
   user_id: melinda.id,
   availability_start: "01-12-2021 10:00",
   availability_end: "01-02-2023 13:30",
+  image_url: 'https://images.unsplash.com/photo-1608855732020-8212e4afd397?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
 )
 room11 = Room.create!(
   title: 'Sea Blue Meeting',
@@ -120,6 +130,7 @@ room11 = Room.create!(
   user_id: melinda.id,
   availability_start: "01-12-2021 10:00",
   availability_end: "01-02-2023 13:30",
+  image_url: 'https://images.unsplash.com/photo-1498503182468-3b51cbb6cb24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
 )
 room12 = Room.create!(
   title: 'Greenhouse Room',
@@ -129,6 +140,7 @@ room12 = Room.create!(
   user_id: melinda.id,
   availability_start: "01-12-2021 10:00",
   availability_end: "01-02-2023 13:30",
+  image_url: 'https://images.unsplash.com/photo-1631130650210-e3863c8d9f8d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80'
 )
 
 puts "Finished!"
